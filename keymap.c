1fb6fb15e22636346daca4ee50de61eda7cfa5e0
digit <digit@abox22.so-net.ne.jp>
2018-12-28 00:58:22 +0900

    Update: Prototype keymap to resolve my mistypo
    Move backspace key from left side to right to resolve my mistypes.
    And add some functionality into Raise + Left hand keys.

--- keymap.c
+++ keymap.c
@@ -68,17 +68,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Tab  |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |Enter |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |   [  |   ]  |   N  |   M  |   ,  |   .  |   /  | Shift|
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Ctrl | Alt  | GUI  | EISU |Lower |Space | Bksp |Space |Space |Raise | KANA | Alt  | Menu | Ctrl |
+   * | Ctrl | Alt  | GUI  | EISU |Lower |Space |Space | Bksp |Space |Raise | KANA | Alt  | App  | Ctrl |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
       KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
       KC_TAB , KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT , \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-      KC_LCTL, KC_LALT, KC_LGUI, EISU,    LOWER,   KC_SPC,  KC_BSPC, KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_RALT, KC_MENU, KC_RCTL \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+      KC_LCTL, KC_LALT, KC_LGUI, EISU,    LOWER,   KC_SPC,  KC_SPC,  KC_BSPC, KC_SPC,  RAISE,   KANA,    KC_RALT, KC_APP , KC_RCTL \
       ),
 
   /* Colemak
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |   1  |   2  |   3  |   4  |   5  |   6  |             |   7  |   8  |   9  |   0  |   -  |   =  |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |  (   |   )  |   '  |   [  |   ]  |  \   |  `   |      |
+   * |      |      |      |      |      |      |      |      |   '  |   [  |   ]  |  \   |  `   |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
    * `-------------------------------------------------------------------------------------------------'
@@ -140,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,\
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                      KC_7,    KC_8,    KC_9   , KC_0,    KC_MINS, KC_EQL, \
-      _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, KC_QUOT, KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV , _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, KC_QUOT, KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV , _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
       ),
 
@@ -148,21 +148,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  | Del  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |      |      |      |      |             | Home |PageUp| Prsc | Sclk | Pause|      |
+   * |      | Mute | Vol- | Vol+ |      |      |             | Home |PageUp| Prsc | Sclk | Pause|      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Caps |      |      |      |      |      |             | Left | Down |  Up  |Right | Bksp |      |
+   * | Caps | Prev | Play | Next |      |      |             | Left | Down |  Up  |Right | Bksp |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      | End  |PageDn| Del  |      |      |      |
+   * |      |      |      |      |      |      |      |      | End  |PageDn| Del  |      | Ins  |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      | Del  |      |      |      |      |      |      |      |
+   * |      |      |      |      |      |      |      | Del  |      |      |      |      |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT( \
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-      _______, _______, _______, _______, _______, _______,                   KC_HOME, KC_PGUP, KC_PSCR, KC_SLCK, KC_PAUS, _______, \
-      KC_CAPS, _______, _______, _______, _______, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, KC_END,  KC_PGDN, KC_DEL,  _______, _______, _______, \
-      _______, _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, _______, _______ \
+      _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,                   KC_HOME, KC_PGUP, KC_PSCR, KC_SLCK, KC_PAUS, _______, \
+      KC_CAPS, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC, _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, KC_END,  KC_PGDN, KC_DEL,  _______, KC_INS,  _______, \
+      _______, _______, _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, _______ \
       ),
 
   /* Adjust (Lower + Raise)
